bca6d3dd4ae3f97a1fd59ee337b5280427168d7a
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-03-24 13:53:24 -0400

    WIP changes to bootstrap

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/astronomerio/commander/api"
-	"github.com/astronomerio/commander/api/v1"
 	"github.com/astronomerio/commander/config"
 	"github.com/astronomerio/commander/kubernetes"
+	"github.com/astronomerio/commander/helm"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -14,6 +15,8 @@ import (
 var (
 	log       = logrus.WithField("package", "cmd")
 	appConfig = config.Get()
+
+	//_ = kubernetes.KubeProvisioner{}
 )
 
 // RootCmd is the commander root command.
@@ -37,22 +40,41 @@ func start() {
 	logger := log.WithField("function", "start")
 	logger.Info("Starting commander")
 
-	// Create new API client and begin accepting requests
-	client := api.NewClient()
-	initDeploymentRouteHandler(client)
-	client.Serve(appConfig.Port)
-}
+	helmClient := helm.New(appConfig.HelmRepo)
 
-func initDeploymentRouteHandler(client *api.Client) {
-	logger := log.WithField("function", "initDeploymentRouteHandler")
-	logger.Debug("Entered initDeploymentRouteHandler")
+	fmt.Println("DERP Helm initialized lets install a release")
+	_, err := helmClient.InstallRelease("astronomer-ee/airflow", "",      appConfig.KubeAirflowNS, map[string]interface{}{})
+	fmt.Println(err)
+	//_, err = helmClient.InstallRelease("astronomer-ee/airflow", "0.1.2", appConfig.KubeAirflowNS, map[string]interface{}{})
+	//fmt.Println(err)
 
-	kubernetesProvisioner, err := kubernetes.NewKubeProvisioner()
+	kubeConfig, err := kubernetes.GetKubeConfig()
 	if err != nil {
 		logger.Panic(err)
 	}
 
-	// Alternate provisioners can be swapped here
-	deploymentRouteHandler := v1.NewDeploymentRouteHandler(kubernetesProvisioner)
-	client.AppendRouteHandler(deploymentRouteHandler)
+	kubeClient, err := kubernetes.New(kubeConfig)
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	// Ensure all namespaces exist
+	kubeClient.Namespace.Ensure(appConfig.KubeCoreNS)
+	kubeClient.Namespace.Ensure(appConfig.KubeAirflowNS)
+	kubeClient.Namespace.Ensure(appConfig.KubeClickstreamNS)
+
+	// Create new API client and begin accepting requests
+	server := api.NewServer()
+	logger.Info(fmt.Sprintf("Starting gRPC server on port %s", appConfig.Port))
+	server.Serve(appConfig.Port)
+}
+
+func getProvisioner() {
+	//kubernetesProvisioner, err := kubernetes.NewKubeProvisioner()
+	//if err != nil {
+	//	logger.Panic(err)
+	//}
+	//
+	//// Alternate provisioners can be swapped here
+	//client.AppendRouteHandler(deploymentRouteHandler)
 }
